1504e493ed7f7cb55a910479b384bb37305fc26f
Dew991120 <dewakapasindutc@gmail.com>
2025-06-08 14:23:50 +0530

    Initial commit for portfolio website

---
+++ src/data/constants.js
@@ -0,0 +1,464 @@
+import kiu from "../images/kiu.png";
+import ranabima from "../images/thurstanofficial_logo.jpg";
+
+import LetsGrow from "../images/LetsGrow.png";
+import deepnix from "../images/deepnix.jpg";
+import SkillSwap from "../images/SkillSwapwebsitelogo.jpg";
+import autodoc from "../images/autodoc.png";
+import vetez from "../images/vetez.jpg";
+
+
+export const Bio = {
+  name: "Dewaka Pasindu",
+  title: "Software Engineer",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/DewakaPasindu",
+  resume:"https://dochub.com/dewaka-pasindu/nO1YzvAwA3zepJkK6L4WP2/dewaka-pasindu-l-cv-resume-pdf",
+  linkedin: "www.linkedin.com/in/dewaka-pasindu-8b147833a",
+  insta: "",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+      },
+      // {
+      //   name: "Next Js",
+      //   image: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg",
+      // },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+      },
+      {
+        name: "JavaScript",
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+      },
+      // {
+      //   name: "Bootstrap",
+      //   image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      // },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        name: "MySQL",
+        image: "https://upload.wikimedia.org/wikipedia/en/d/dd/MySQL_logo.svg",
+
+      },
+     
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+      },
+      {
+        name: "GitHub",
+        image: "https://img.icons8.com/ios-filled/50/ffffff/github.png"
+      },
+      {
+        name: "Docker",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+      },
+      {
+        name: "VS Code",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+      },
+      {
+        name: "Postman",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg",
+      },
+      {
+        name: "Figma",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+      },
+    ],
+  }
+  
+];
+
+export const experiences = [
+//   {
+//     id: 0,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+//     role: "DevOps & Fullstack Engineering Intern",
+//     company: "Flipr Inovations Pvt. Ltd.",
+//     date: "Aug 2023 - July 2023",
+//     desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+//     skills: [
+//       "Docker",
+//       "Terraform",
+//       "AWS",
+//       "EC2",
+//       "Portainer",
+//       "Nginx",
+//       "JavaScript",
+//       "TypeScript",
+//       "Node Js",
+//       " Next Js",
+//     ],
+//     doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+//   },
+//   {
+//     id: 0,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+//     role: "Frontend Engineer Intern",
+//     company: "Neurobit",
+//     date: "June 2023 - Present",
+//     desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+//     skills: [
+//       "ReactJS",
+//       "Redux",
+//       "NodeJs",
+//       "Material UI",
+//       "HTML",
+//       "CSS",
+//       "JavaScript",
+//     ],
+//     doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+//   },
+//   {
+//     id: 1,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+//     role: "Fullstack Externship",
+//     company: "Flipr",
+//     date: "June 2023 - July 2023",
+//     desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+//     skills: [
+//       "ReactJS",
+//       "Redux",
+//       "NodeJs",
+//       "Material UI",
+//       "HTML",
+//       "CSS",
+//       "JavaScript",
+//       "Docker",
+//       "AWS",
+//       "MongoDB",
+//     ],
+//   },
+//   {
+//     id: 2,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+//     role: "Open Source Contributor ",
+//     company: "GirlScript Summer of Code",
+//     date: "May 2023 - Present",
+//     desc: "Contributing to different open-source projects and learn from industry experts",
+//     doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+//   },
+//   {
+//     id: 3,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+//     role: "Android Developer",
+//     company: "DSC KIIT",
+//     date: "Nov2021 - Present",
+//     desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+//     skills: [
+//       "Leadership",
+//       "Mobile Application Development",
+//       "Kotlin",
+//       "XML",
+//       "Figma",
+//     ],
+//   },
+//   {
+//     id: 4,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+//     role: "Android Developer Intern",
+//     company: "Rudraksha Welffare Foundation",
+//     date: "June 2021 - Oct 2021",
+//     desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+//     skills: [
+//       "Android",
+//       "Java",
+//       "Kotlin",
+//       "XML",
+//       "Node Js",
+//       "Cloud Firestore",
+//       "Firebase",
+//       "Figma",
+//     ],
+//     doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+//   },
+ ];
+
+export const education = [
+  {
+    id: 0,
+    img: kiu,
+    school: "Kaatsu International University, Battaramulla",
+    date: "Oct 2022 - Oct 2026",
+    grade: "0 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Software Engineering at Kaatsu International University, Battaramulla. I have completed 5 semesters. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a committee member of the Faculty of Computer Science and Engineering Circle(FOCSEC) at KIU.",
+    degree: "Bsc (Hons) in Software Engineering",
+  },
+  {
+    id: 1,
+    img: ranabima,
+    school: "Thurstan College, Colombo 03",
+    date: "Feb 2010 - Aug 2018",
+    grade: "O/L - 7 A's  | A/L - 3 C's",
+    desc: "I completed my high School education at Thurstan College, where I studied Combined Mathematics.",
+    degree: "Combined Mathematics",
+  },
+  
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Let's Grow",
+    date: "Jan 2025 - Present",
+    description:
+      "Let's Grow, a smart and user-friendly platform for investors and Startups. Our goal is help to find the stratups and new ideas for the investors, while also giving proper proposal to investors, and reach two parties in one platform. within their interested feilds first.",
+    image:LetsGrow,    
+    tags: [
+      "React Js",
+      "HTML",
+      "CSS",
+      "MySQL",
+      
+    ],
+    category: "web app",
+    github: "https://github.com/Chamika-Herath/travel_compass_frontend",
+    // webapp: "https://trackify.duckdns.org",
+    
+    // member: [
+    //   {
+    //     name: "Visal Hewage",
+    //     img: `https://github.com/Visal2000.png`,
+    //     //linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    //     github: "https://github.com/Visal2000",
+    //   },
+    //   {
+    //     name: "Tharusha Ishan",
+    //     img: `https://github.com/Tharusha553.png`,
+    //     //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+    //     github: "https://github.com/Tharusha553",
+    //   },
+    //   {
+    //     name: "Erandi Hansika",
+    //     img: `https://github.com/Erandi9924.png`,
+    //     //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+    //     github: "https://github.com/Erandi9924",
+    //   },
+    //   {
+    //     name: "Sachintha Jayaweera",
+    //     img: `https://github.com/Sachinthajayaweera.png`,
+    //     //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+    //     github: "https://github.com/Sachinthajayaweera",
+    //   },
+    //   {
+    //     name: "Chamika Herath",
+    //     img: `https://github.com/Chamika-Herath.png`,
+    //     //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+    //     github: "https://github.com/Chamika-Herath",
+    //   },
+    // ],
+  },
+
+  {
+    id: 3,
+    title: "Deepnix",
+    date: "Feb 2025 - Present",
+    description:"Deepnix is the website of our suggested Software Company Project",
+    image:deepnix,
+    tags: [
+      "React Js",
+      "HTML",
+      "CSS",
+      "Node Js",
+      "Express Js",
+      "MySQL",
+    ],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Buckoid-Android-App",
+    webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+
+
+  {
+    id: 0,
+    title: "SkillSwap",
+    date: "Jul 2024 - Nov 2024",
+    description:
+      "SkillSwap is a web-based full stack platform designed to connect individuals who want to share and exchange skills. The platform offers a rich set of features including, Skill-Based Matching, Interactive Learning Modules, Gamified Experience, Modern UI/UX for users.",
+    image:SkillSwap,
+    tags: [
+      "Node Js",
+      "React Js",
+      "HTML",
+      "CSS",
+      "MySQL",
+    ],
+    category: "web app",
+    github: "https://github.com/DewakaPasindu/SkillSwap",
+    // webapp: "https://podstream.netlify.app/",
+    
+    // member: [
+    //   {
+    //     name: "Dewaka Pasindu",
+    //     img: `https://github.com/DewakaPasindu.png`,
+    //     //linkedin: "https://www.linkedin.com/in/dewaka-pasindu-8b147833a/",
+    //     github: "https://github.com/DewakaPasindu",
+    //   },
+    // ],
+  },
+  {
+    id: 1,
+    title: "AutoDOC",
+    date: "Jan 2024 - May 2024",
+    description:
+      "AutoDOC website is a digital platform that streamlines the process of booking, tracking, and managing vehicle maintenance and repair services. It allows customers to schedule service appointments, view service history, and connect with service providers efficiently, improving overall customer experience and workshop productivity.",
+    image:autodoc,
+    tags: [
+      "React Js",
+      "HTML",
+      "CSS",
+      "Node Js",
+      "Express Js",
+      "MySQL",
+    ],
+    category: "web app",
+    github: "https://github.com/Sachinthajayaweera/autodoc",
+    // webapp: "https://vexa-app.netlify.app/",
+    member: [
+      {
+        name: "Visal Hewage",
+        img: `https://github.com/Visal2000.png`,
+        //linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/Visal2000",
+      },
+      {
+        name: "Tharusha Ishan",
+        img: `https://github.com/Tharusha553.png`,
+        //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/Tharusha553",
+      },
+      {
+        name: "Erandi Hansika",
+        img: `https://github.com/Erandi9924.png`,
+        //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/Erandi9924",
+      },
+      {
+        name: "Sachintha Jayaweera",
+        img: `https://github.com/Sachinthajayaweera.png`,
+        //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/Sachinthajayaweera",
+      },
+      {
+        name: "Chamika Herath",
+        img: `https://github.com/Chamika-Herath.png`,
+        //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/Chamika-Herath",
+      },
+    ],
+  },
+
+
+  {
+    id: 2,
+    title: "VetEZ",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "VetEZ is a software application designed to streamline operations and enhance pet vaccination management within veterinary clinics. It aims to offer a user-friendly and comprehensive platform that simplifies administrative tasks, improves communication with pet owners, and ultimately contributes to better pet health outcomes.",
+    image:vetez,
+    tags: [
+      "React Js",
+      "HTML",
+      "CSS",
+      "Node Js",
+      "Express Js",
+      "MySQL",
+    ],
+    category: "web app",
+    
+    github: "https://github.com/Visal2000/veterinary_system",
+    //webapp: "https://brain-tumor.netlify.app/",
+    member: [
+      {
+        name: "Visal Hewage",
+        img: `https://github.com/Visal2000.png`,
+        //linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/Visal2000",
+      },
+      {
+        name: "Tharusha Ishan",
+        img: `https://github.com/Tharusha553.png`,
+        //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/Tharusha553",
+      },
+      {
+        name: "Yasantha Ravindu",
+        img: `https://github.com/Ravindu9EV.png`,
+        //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/Ravindu9EV",
+      },
+      
+      {
+        name: "Sachintha Jayaweera",
+        img: `https://github.com/Sachinthajayaweera.png`,
+        //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/Sachinthajayaweera",
+      },
+      {
+        name: "Chamika Herath",
+        img: `https://github.com/Chamika-Herath.png`,
+        //linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/Chamika-Herath",
+      },
+    ],
+  },
+  
+ 
+];
+
+
